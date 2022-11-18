85cbce78e65c55301c70bad04dbfb7d91a0a755b
jpizzati-uvl <jpizzati@uvltd.com>
2022-11-18 15:46:03 +0100

    upgrade deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
+import { createRequire } from "node:module";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+const require = createRequire(import.meta.url);
 const packageJson = require("./package.json");
 
 export default [
