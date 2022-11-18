eebbf324db85cfe6793dca4757cc335f33b7a2c7
jpizzati-uvl <jpizzati@uvltd.com>
2022-11-18 18:10:32 +0100

    adding test support

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,15 +1,33 @@
+// Needed to import package.json after rollup 3 upgrade
 import { createRequire } from "node:module";
+
+// Uses the node resolution algorithm for modules
 import resolve from "@rollup/plugin-node-resolve";
+
+// Converts commonjs modules to ES6 modules
 import commonjs from "@rollup/plugin-commonjs";
+
+// Teaches rollup how to process Typescript files
 import typescript from "@rollup/plugin-typescript";
+
+// CSS Processing
+import postcss from "rollup-plugin-postcss";
+
+// rollup your .d.ts files
 import dts from "rollup-plugin-dts";
+
+// Minify bundle
+import terser from "@rollup/plugin-terser";
+
+// Prevents module duplication
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const require = createRequire(import.meta.url);
 const packageJson = require("./package.json");
 
 export default [
   {
-    input: "src/index.ts",
+    input: "src/index.ts", // Entry point from where we export all modules
     output: [
       {
         file: packageJson.main,
@@ -23,9 +41,12 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+      terser(),
     ],
   },
   {
