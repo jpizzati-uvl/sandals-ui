249f19e18cca93febb3242729667476dec81fe2a
jpizzati-uvl <jpizzati@uvltd.com>
2023-01-13 13:43:39 +0100

    0.1.54

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import './../src/styles/master.scss';
+import '!style-loader!css-loader!sass-loader!./../src/styles/master.scss';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
