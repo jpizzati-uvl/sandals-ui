01f6ec05100a0ab0adbb4dded68e31b51ff30598
jpizzati-uvl <jpizzati@uvltd.com>
2023-02-08 12:33:16 +0100

    0.1.1

--- utils/optimization/resolve.ts
+++ utils/optimization/resolve.ts
@@ -4,44 +4,46 @@ interface ResolveInterface {
     updateImage: (element: HTMLImageElement | null, force: boolean) => void;
   };
 }
-
 const resolve: ResolveInterface = {};
-const resolveScript = document.createElement('script');
 
-resolveScript.defer = true;
-resolveScript.type = 'text/javascript';
-resolveScript.src =
-  'https://resolve.sandals.com/zap/dhtml/com.liquidpixels.Resolve.jsr';
-document.head.appendChild(resolveScript);
+if (typeof window !== 'undefined') {
+  const resolveScript = document.createElement('script');
 
-resolveScript.onload = () => {
-  (() => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore: Unreachable code error
-    // eslint-disable-next-line no-undef
-    resolve.optimizer = new com.liquidpixels.Resolve({
-      server: 'resolve.sandals.com',
-      uri: 'sri',
-      port: '80',
-      base: 'https://',
-      imageFit: 'containerWidth',
-      lazyLoad: true,
-      advancedOptions: {
-        cacheThreshold: 500,
-      },
+  resolveScript.defer = true;
+  resolveScript.type = 'text/javascript';
+  resolveScript.src =
+    'https://resolve.sandals.com/zap/dhtml/com.liquidpixels.Resolve.jsr';
+  document.head.appendChild(resolveScript);
 
-      output: (
-        browser: string,
-        image: {
-          format: string;
-        }
-      ) => {
-        return {
-          format: ['webp', image.format ? image.format : 'jpg'],
-        };
-      },
-    });
-  })();
-};
+  resolveScript.onload = () => {
+    (() => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore: Unreachable code error
+      // eslint-disable-next-line no-undef
+      resolve.optimizer = new com.liquidpixels.Resolve({
+        server: 'resolve.sandals.com',
+        uri: 'sri',
+        port: '80',
+        base: 'https://',
+        imageFit: 'containerWidth',
+        lazyLoad: true,
+        advancedOptions: {
+          cacheThreshold: 500,
+        },
+
+        output: (
+          browser: string,
+          image: {
+            format: string;
+          }
+        ) => {
+          return {
+            format: ['webp', image.format ? image.format : 'jpg'],
+          };
+        },
+      });
+    })();
+  };
+}
 
 export default resolve;
